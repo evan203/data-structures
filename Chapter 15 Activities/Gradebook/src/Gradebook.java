e07891b7aa466832ab63fae03bf2d4a89d9966f8
evan203 <evancreeper203@gmail.com>
2023-09-28 14:19:56 -0500

    Done with pair lab

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,4 @@
-import java.util.Scanner;
-. . .
+import java.util.*;
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -9,34 +8,44 @@ public class Gradebook
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-
-        . . .
+        Map<String, String> map = new HashMap<>();
 
         boolean done = false;
         while(!done)
         {
             System.out.println("A)dd R)emove M)odify P)rint Q)uit");
             String input = in.next().toUpperCase();
-            if (input.equals("Q"))
+            if (input.equals("A"))
             {
+                System.out.print("Student Name: ");
+                input = in.next().toUpperCase();
+                System.out.print("Course grade: ");
+                map.put(input, in.next().toUpperCase());
+            } 
+            else if (input.equals("R"))
+            {
+                System.out.print("Student Name: ");   
+                input = in.next().toUpperCase();
+                map.remove(input);            
+            } 
+            else if (input.equals("M"))
+            {
+                System.out.print("Student Name: ");   
+                input = in.next().toUpperCase();
+                System.out.print("Course grade: ");
+                map.put(input, in.next().toUpperCase());
+                
+            } 
+            else if (input.equalsIgnoreCase("P"))
+            {
+                for (Map.Entry<String, String> me :map.entrySet()) {
+                    System.out.print(me.getKey() + ":");
+                    System.out.println(me.getValue());
+                }
+                
+            } 
+            else
                 done = true;
-            } else if (input.equals("A"))
-            {
-                . . .
-
-            } else if (input.equals("R"))
-            {
-                . . .
-            } else if (input.equals("M"))
-            {
-                . . .
-            } else if (input.equalsIgnoreCase("P"))
-            {
-                . . .
-            } else
-            {
-                done = true;
-            }
         }
     }
 }
