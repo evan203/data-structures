9eae94d74e68a78c35f85ed9fae6ee1d8f1c236f
evan203 <evancreeper203@gmail.com>
2023-10-05 14:19:58 -0500

    flood fill

--- Chapter 15 Activities/FloodFillQueue/src/Grid.java
+++ Chapter 15 Activities/FloodFillQueue/src/Grid.java
@@ -1,16 +1,31 @@
-. . .
+import java.util.*;
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    Stack<Pair> pairs;
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        pairs = new Stack<>();
+        pairs.push(new Pair(row, column));
+        for (int i = 0; !pairs.isEmpty(); i++)
+        {
+            Pair top = pairs.pop();
+            if (top.getRow() < SIZE  && top.getRow() >= 0 && top.getColumn() < SIZE && top.getColumn() >= 0
+            && pixels[top.getRow()][top.getColumn()] == 0)
+            {
+                pixels[top.getRow()][top.getColumn()] = i;
+                pairs.push(new Pair(top.getRow()-1, top.getColumn()));
+                pairs.push(new Pair(top.getRow(), top.getColumn()-1));
+                pairs.push(new Pair(top.getRow()+1, top.getColumn()));
+                pairs.push(new Pair(top.getRow(), top.getColumn()+1));
+            }
+        }
+        System.out.println(this.toString());
     }
 
     public String toString()
